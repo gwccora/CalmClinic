8da05a160a93359e5c36d2284c35182d18c9cc29
Twitter <twitter@Twitters-MacBook-Pro.local>
2017-08-15 15:42:58 -0700

    added talk with bear webpage

---
+++ ccwhatswrong.js
@@ -0,0 +1,19 @@
+function changeSize() {
+    var width = parseInt($("#Width").val());
+    var height = parseInt($("#Height").val());
+
+    $("#Demo").width(width).height(height);
+
+    // update scrollbars
+    $('#Demo').perfectScrollbar('update');
+
+    // or even with vanilla JS!
+    Ps.update(document.getElementById('Demo'));
+}
+
+$(function() {
+    $('#Demo').perfectScrollbar();
+
+    // with vanilla JS!
+    Ps.initialize(document.getElementById('Demo'));
+});
